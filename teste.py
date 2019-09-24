5726c002ca68476d3644d88f93c368beb1ee3139
gleissonjoaquim3 <gleissoncg2@gmail.com>
2019-09-24 15:47:42 -0300

    Incrementação do código

--- teste.py
+++ teste.py
@@ -1,6 +1,24 @@
-import datetime as date*
+# Importa do módulo datatime
+import datetime
 
-nome = input('Ola ! Me dia qual o seu nome : ')
+# Recebe o nome do usúario
+nome = input('Ola! Me diga qual o seu nome : ')
+
+#Recebe a idade do usúario
 idade = int(input('Qual a sua idade ? '))
+
 # Recebe um número com o tamanho do nome digitado
 tm = (len(nome))
+
+# Recebe uma confirmação do usuário
+conf = input('Ola {} !! Sua idade é de {} anos ? (S / N) :  '.format(nome, idade)).upper
+
+# Recebe a resposta S ou N
+resp = conf
+
+# Condição
+if conf == ('S') :
+    print('Que bom que está certo :)')
+else :
+    nome = input ('Qual deseja alterar o nome ou a idade ? ')
+    
