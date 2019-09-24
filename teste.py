542516da941825997398e63cad810da23578ae79
gleissonjoaquim3 <gleissoncg2@gmail.com>
2019-09-24 14:57:15 -0300

    Código em python

---
+++ teste.py
@@ -0,0 +1,6 @@
+import datetime as date*
+
+nome = input('Ola ! Me dia qual o seu nome : ')
+idade = int(input('Qual a sua idade ? '))
+# Recebe um número com o tamanho do nome digitado
+tm = (len(nome))
