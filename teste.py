510c8a73d3e27892367fca6c078cec3bbbe96210
gleissonjoaquim3 <gleissoncg2@gmail.com>
2019-09-26 17:24:57 -0300

    Alinhamentos

--- teste.py
+++ teste.py
@@ -3,7 +3,7 @@ nome = input('Ola! Me diga qual o seu nome : ')
 #Recebe a idade do usúario
 idade = int(input('Qual a sua idade ? '))
 # Printa o nome e idade do usuário
-print('Ola {} !! Sua idade é de {} anos !'.format(nome, idade))
+print(f'Ola {nome} !! Sua idade é de {idade} anos !')
 
 resp = str(input('Seu nome e idade estão corretos ? (S / N) ? ')).upper
 # Condição
