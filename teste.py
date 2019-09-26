0a0ce4d8a5843d4dd0cb98b2afa2d6889684969a
gleissonjoaquim3 <gleissoncg2@gmail.com>
2019-09-26 17:17:45 -0300

    Atualização dos códigos

--- teste.py
+++ teste.py
@@ -1,24 +1,23 @@
 # Recebe o nome do usúario
 nome = input('Ola! Me diga qual o seu nome : ')
-
 #Recebe a idade do usúario
 idade = int(input('Qual a sua idade ? '))
+# Printa o nome e idade do usuário
+print('Ola {} !! Sua idade é de {} anos !'.format(nome, idade))
 
-# Recebe um número com o tamanho do nome digitado
-tm = (len(nome))
-
-# Recebe uma confirmação do usuário
-conf = input('Ola {} !! Sua idade é de {} anos ! \nAs informações estão corretas ? (S / N) :  '.format(nome, idade)).upper
-print (-=*10)
-# Condição 
+resp = str(input('Seu nome e idade estão corretos ? (S / N) ? ')).upper
+# Condição
 #ainda não funciona...
-if  conf == 'S' :
+if  resp == 'S' :
             print('Que bom que está certo :)')
+else :
+    resp1 = input('O que deseja alterar ? (Nome / Idade) ? ')
+#Ainda não funciona
+if resp1 == 'Nome' :
+    nome1 = input('Digite seu nome : ')
+    print(f'Muito bem {nome1} ! Seu nome foi aterado !')
+else  :
+    nome2 = input('Digite sua idade : ')
+    print(f'Muito bem {nome1} ! Sua {nome2} foi aterado !')
 
-elif conf == ('N') :
-    nome = input('O que deseja alterar ? O nome ou a idade ? ').upper
-    if nome == ('NOME') :
-       nome2 = input('Digite seu nome : ')
-       print(f'Nome alterado !\Ola {nome2} seja bem-vindo !')
-   
 
