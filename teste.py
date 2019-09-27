0db06e0bdef8e651045dd9cd2cd2d25a8773c968
Gleisson Jesuino Joaquim Cardoso <gleissoncg2@gmail.com>
2019-09-27 00:15:07 -0300

    Código arrumado e funcionando

--- teste.py
+++ teste.py
@@ -4,20 +4,21 @@ nome = input('Ola! Me diga qual o seu nome : ')
 idade = int(input('Qual a sua idade ? '))
 # Printa o nome e idade do usuário
 print(f'Ola {nome} !! Sua idade é de {idade} anos !')
-
+#Recebe resposta da pergunta
 resp = str(input('Seu nome e idade estão corretos ? (S / N) ? '))
-# Condição
-#ainda não funciona...
-if  resp == 'S' :
-            print('Que bom que está certo :)')
+#Condição para resposta negativa
+if  resp == 'N' :
+    nome1 = input('O que deseja alterar ? (NOME / IDADE) ? ')
+    if nome1 == 'NOME':
+        nome2 = input('Digite seu nome : ')
+        print(f'Muito bem {nome2} ! Seu nome foi alterado !' )
+    if nome1 == 'IDADE' :
+        nome3 = input('Digite dua idade : ')
+        print(f'Muito bem {nome} ! Sua idade foi alterada para {nome3} anos !' )
+#Necessita de melhorias
+#    if nome1 != 'NOME' and 'IDADE' :
+#       print('Não endenti...')
 else :
-    resp1 = input('O que deseja alterar ? (Nome / Idade) ? ')
-#Ainda não funciona
-if resp1 == 'Nome' :
-    nome1 = input('Digite seu nome : ')
-    print(f'Muito bem {nome1} ! Seu nome foi aterado !')
-else  :
-    nome2 = input('Digite sua idade : ')
-    print(f'Muito bem {nome1} ! Sua {nome2} foi aterado !')
+    print('Que bom que está certo :)')
 
 
