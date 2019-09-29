e01c2f5fcaebc2731f8fc5aaec77c0d42ee5955b
gleissonjoaquim3 <gleissoncg2@gmail.com>
2019-09-29 17:15:43 -0300

    Adicionando funcções

--- Comentários.py
+++ Comentários.py
@@ -1,8 +1,8 @@
 #Exemplo de comentário
-print('Linha 1')  
-print('Linha 3')
-print('Linha 5')
-print('Linha 6')
-print('Linha 8')
-print('Linha 10')
+print('Linha 1'.upper())  
+print('Linha 3'.lower())
+print('Linha 5'.capitalize())
+print('Linha 6'.center(0))
+print('Linha 8'.strip())
+print('Linha 10'.split())
 
