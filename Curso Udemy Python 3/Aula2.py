707540e290d438d3f9e70131634246e5551e094f
gleissonjoaquim3 <gleissoncg2@gmail.com>
2019-10-02 09:47:26 -0300

    Updates

---
+++ Curso Udemy Python 3/Aula2.py
@@ -0,0 +1,21 @@
+'''
+Tipo de dados
+str - string - Textos - alfanumérico 'Assim' ou "Assim" 
+int - inteiro - 12345 0 -10 -50 -60 -1550000
+float - real/ponto flutuante - 10.50 1.5 -10.390
+bool - booleano / lógico - true/false 100 == 100 
+type - retorna o tipo do valor
+== pergunta ao python/ comparação
+'''
+'''
+print(type('Luiz'))
+print(type(24.32))
+print(type(10))
+print(10==10, type(10==10))
+print('10', type('10'), type(int('10')))
+print('Luiz' + 'Carvalho')
+'''
+print('Gleisson', type('Gleisson'))
+print(32, type('32'))
+print(1.80, type(1.80))
+print(32>18, bool(32>18))
---
+++ Curso Udemy Python 3/aula3.py
@@ -0,0 +1,22 @@
+# Operadores numéricos
+'''
++, - *, /, //, **, %, ()
+
+print('Multiplicação *', 10*10)
+print('Adição +', 10+10)
+print('Subtração - ', 10-10)
+print('Divisão', 10/10)
+print('Luiz' + 'Otavio')  # Concatenação
+'''
+
+def saudacao(msg='Ola', nome='usuaruo'):
+    nome = nome.replace('e','3')
+    msg = msg.replace('f','2')
+    print(msg, nome)
+
+saudacao(nome='Zezinho', msg='FalaHi')
+saudacao('Hello', 'Ok')
+saudacao('teste', 'tester')
+saudacao('res', 'redfd')
+
+
